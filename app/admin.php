a3d51408114883b2ce32eb16a59f007137ea4dc5
Paulo Henrique Martins <paulovelho@paulovelho.com>
2016-08-07 02:45:55 +0200

    admin login

--- app/admin.php
+++ app/admin.php
@@ -1,7 +1,20 @@
 <?php
-  include("inc/global.php");
-  include($magrathea_path."/MagratheaAdmin.php"); // $magrathea_path should already be declared
- 
-  $admin = new MagratheaAdmin(); // adds the admin file
-  $admin->Load(); // load!
+	include("inc/global.php");
+	include($magrathea_path."/MagratheaAdmin.php"); //  should already be declared
+
+	class LoginController extends MagratheaController {
+		public static function Login(){
+			if(@$_GET["error"] == "login_error")
+				self::GetSmarty()->assign("message", "Login or password incorrect!");
+			self::GetSmarty()->display("login.html");
+		}
+	}
+
+	if(!empty($_SESSION["magrathea_user"])) {
+		$admin = new MagratheaAdmin(); // adds the admin file
+		$admin->Load(); // load!
+	} else {
+		LoginController::Login();
+	}
+
 ?>
